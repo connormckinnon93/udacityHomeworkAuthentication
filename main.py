37190b8f545e1f2439cf00b6dccf6339a1cbd521
Connor McKinnon <connor.mckinnon@senecacollege.ca>
2017-05-14 15:22:18 -0400

    add user model and associated methods

--- main.py
+++ main.py
@@ -21,15 +21,20 @@ def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
 
+# Stuff to secure Cookies
+
 
 def make_secure_val(val):
     return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
 
 
 def check_secure_val(secure_val):
+    # Use pipe not comma to avoid GAE issues
     val = secure_val.split('|')[0]
     if secure_val == make_secure_val(val):
         return val
+
+# Handler object inherited by every controller
 
 
 class Handler(webapp2.RequestHandler):
@@ -67,3 +72,60 @@ class Handler(webapp2.RequestHandler):
         webapp2.RequestHandler.initialize(self, *a, **kw)
         uid = self.read_secure_cookie('user_id')
         self.user = uid and User.by_id(int(uid))
+
+
+class MainPage(Handler):
+
+    def get(self):
+        self.write("Hello, Udacity!")
+
+# User Functions and Models
+
+
+def make_salt(length=5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+
+def make_pw_hash(name, pw, salt=None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s|%s' % (h, salt)
+
+
+def valid_pw(name, password, h):
+    salt = h.split('|')[1]
+    return h == make_pw_hash(name, password, salt)
+
+
+def users_key(group='default'):
+    return db.Key.from_path('users', group)
+
+
+class User(db.Model):
+    name = db.StringProperty(required=True)
+    pw_hash = db.StringProperty(required=True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid, parent=users_key())
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email=None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(parent=users_key(),
+                    name=name,
+                    pw_hash=pw_hash,
+                    email=email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
